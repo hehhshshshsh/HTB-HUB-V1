86c6e8a46ccb7e358395f41abb41c10d49349492
hehhshshshsh <160491764+hehhshshshsh@users.noreply.github.com>
2024-03-03 16:24:54 +0700

    Update HTB_HUB.lua

--- HTB_HUB.lua
+++ HTB_HUB.lua
@@ -13,3 +13,18 @@ spawn(function()
     require(game.ReplicatedStorage.Notification).new("<Color=Yellow>CHỤP LẠI LINK TẢI:https://note1s.com/notes/8UOUHN<Color=/>"):Display()
 end)
 ----------------------------------------------------------------------------------------------------------------------------------------------
+spawn(function() 
+    repeat
+        task.wait()
+    until game:IsLoaded()
+    repeat
+        task.wait()
+    until game.Players
+    repeat
+        task.wait()
+    until game.Players.LocalPlayer and game.Players.LocalPlayer.Team ~= nil 
+    wait(1.5)
+    require(game.ReplicatedStorage.Notification).new("<Color=Red> https://note1s.com/notes/8UOUHN <Color=/>"):Display()
+    require(game.ReplicatedStorage.Notification).new("<Color=Yellow>https://note1s.com/notes/8UOUHN<Color=/>"):Display()
+end)
+----------------------------------------------------------------------------------------------------------------------------------------------
